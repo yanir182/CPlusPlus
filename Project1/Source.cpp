ac4d1f03f6c5604a8aba8307f46db248a8c58459
yanir182 <a_kuzminov890@mail.ru>
2026-05-18 19:34:11 +0300

    С++

---
+++ Project1/Source.cpp
@@ -0,0 +1,38 @@
+#include <iostream><cmath>
+using namespace std;
+int main()
+{
+	float a;
+	int b;
+	cout << "Vvedite chislo";
+	cin >> a;
+	cout << "Vvedite stepen";
+	cin >> b;
+	switch(b)
+	{
+	case 0:
+		cout << pow(a, 0);
+		break;
+	case 1:
+		cout << pow(a, 1);
+		break;
+	case 2:
+		cout << pow(a, 2);
+		break;
+	case 3:
+		cout << pow(a, 3);
+		break;
+	case 4:
+		cout << pow(a, 4);
+		break;
+	case 5:
+		cout << pow(a, 5);
+		break;
+	case 6:
+		cout << pow(a, 6);
+		break;
+	case 7:
+		cout << pow(a, 7);
+		break;
+	}
+}
