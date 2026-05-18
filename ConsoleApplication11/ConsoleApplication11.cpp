adc16e44d8431faa37379160e5a4b6cfc216d746
yanir182 <a_kuzminov890@mail.ru>
2026-05-18 19:27:55 +0300

    С++

---
+++ ConsoleApplication11/ConsoleApplication11.cpp
@@ -0,0 +1,21 @@
+﻿#include <iostream>
+using namespace std;
+int main()
+{
+    setlocale(0, "Russian");
+    int a;
+    cout << "Введите высоту\n";
+    cin >> a;
+    for (int i=0; i <= a; i++)
+    {
+        for (int j=0; j <= a; j++)
+        {
+            if (i + j >= a && i >= j)
+                cout << "^";
+            else
+                cout << " ";
+        }
+        cout << "\n";
+    }
+    cout << "\n";
+}
---
+++ ConsoleApplication9/ConsoleApplication9.cpp
@@ -0,0 +1,10 @@
+﻿#include <iostream>
+using namespace std;
+int main()
+{
+	setlocale(0, "Russian");
+	int a, b, c;
+	cout << "Введите 3-значное число";
+	cin >> a, b;
+	cout << b, a ;
+}
